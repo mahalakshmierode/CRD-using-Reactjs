c498c6266aa143c136cd07fac5c24f72bfce2045
mahalakshmierode <ksmahalakshmi76@gmail.com>
2024-03-28 00:01:38 -0700

    successfully

---
+++ App4.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import Create from './Components1/Create';
+import Update from './Components1/Update';
+import Read from './Components1/Read';
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
+function App() {
+  return (
+    <div className="main">
+      <h2>CURD OPERTION</h2>
+      <BrowserRouter>
+      <Routes>
+        <Route exact path="/Create" element={<Create />}/>
+        <Route exact path="/update/:id" component={Update} /> {/* Route for Update with ID parameter */}
+        <Route exact path="/Read" element={<Read />}/>
+        </Routes>
+        </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
